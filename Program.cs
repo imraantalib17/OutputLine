222aeec61ba84039f25321c7c6141d6d49a4244d
Imraan Talib <imraantalib17@gmail.com>
2026-09-12 16:23:29 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿while (true)
+{
+    Console.WriteLine();
+
+    string text1 = Console.ReadLine();
+    string text2 = Console.ReadLine();
+    string text3 = Console.ReadLine();
+    string text4 = Console.ReadLine();
+    string text5 = Console.ReadLine();
+
+    Console.WriteLine();
+
+    Console.WriteLine($"{text1}");
+    Console.WriteLine($"{text2}");
+    Console.WriteLine($"{text3}");
+    Console.WriteLine($"{text4}");
+    Console.WriteLine($"{text5}");
+}
